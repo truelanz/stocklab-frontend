86e104de82df965a388d7d12ef16d42e8c5cc62e
truelanz <devalanoliveira@gmail.com>
2025-10-08 23:00:04 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/DashBoardComponent.jsx
+++ src/components/DashBoardComponent.jsx
@@ -1,32 +1,43 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./DashBoardComponent.css";
-import kitImage from "../assets/kit-de-primeiros-socorros.png";
+import produtosImg from "../assets/produtos.png";
+import servicosImg from "../assets/servicos.png";
+import relatoriosImg from "../assets/relatorios.png";
+import clientesImg from "../assets/clientes.png";
+import funcionariosImg from "../assets/funcionarios.png";
+import configuracoesImg from "../assets/configuracoes.png";
 
 function DashboardComponent() {
-  const [sections] = useState([
-    "Produtos",
-    "Serviços",
-    "Relatórios",
-    "Clientes",
-    "Funcionários",
-    "Configurações"
-  ]);
+  const sections = [
+    { name: "Produtos", img: produtosImg,  path: "/products" },
+    { name: "Serviços", img: servicosImg },
+    { name: "Relatórios", img: relatoriosImg },
+    { name: "Clientes", img: clientesImg },
+    { name: "Funcionários", img: funcionariosImg },
+    { name: "Configurações", img: configuracoesImg },
+  ];
 
   return (
     <div className="dashboard-page">
       <h1 className="dashboard-title">Painel de Controle</h1>
 
       <div className="dashboard-grid">
-        {sections.map((section) => (
-          <div key={section} className="dashboard-card">
-            <h2>{section}</h2>
-            <img
-              src={kitImage}
-              alt="produtos"
-              style={{ width: "150px", height: "150px", marginTop: "1rem" }}
-            />
-          </div>
-        ))}
+        {sections.map((section) =>
+          section.path ? (
+            <Link key={section.name} to={section.path} className="dashboard-card-link">
+              <div className="dashboard-card">
+                <img src={section.img} alt={section.name} className="dashboard-icon" />
+                <h2 >{section.name}</h2>
+              </div>
+            </Link>
+          ) : (
+            <div key={section.name} className="dashboard-card">
+              <img src={section.img} alt={section.name} className="dashboard-icon" />
+              <h2>{section.name}</h2>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import DashboardComponent from "../components/DashBoardComponent";
+
 
 function Dashboard() {
   return (
